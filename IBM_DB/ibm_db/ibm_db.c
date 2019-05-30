39062beeb535bb74593a8c22a5a1505e863a9bc4
Kevin Adler <kadler@us.ibm.com>
2019-05-30 11:02:24 -0500

    Update version to 2.0.5.11

--- IBM_DB/ibm_db/ibm_db.c
+++ IBM_DB/ibm_db/ibm_db.c
@@ -21,7 +21,7 @@
 +--------------------------------------------------------------------------+
 */
 
-#define MODULE_RELEASE "2.0.5.1"
+#define MODULE_RELEASE "2.0.5.11"
 
 #include <Python.h>
 #include <datetime.h>
